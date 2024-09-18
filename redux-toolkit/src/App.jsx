515097a182c3d9e52de9652ad3f040c07e145720
Tushar Ranjan <thrrnjn1103@gmail.com>
2024-09-18 17:16:53 +0530

    redux toolkit

---
+++ redux-toolkit/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <h1>Redux Todo </h1>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
